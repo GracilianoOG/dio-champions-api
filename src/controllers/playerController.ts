005397c8cab7f52aa93430bc0fb8db070452b0bd
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-13 12:27:47 -0300

    Rename data to response

--- src/controllers/playerController.ts
+++ src/controllers/playerController.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
 import { getAllPlayersService } from "../services/playersService";
-import { OK } from "../utils/httpHelper";
 
 export const getPlayer = async (req: Request, res: Response) => {
-  const data = await getAllPlayersService();
-  const { statusCode, body } = await OK(data);
+  const response = await getAllPlayersService();
+  const { statusCode, body } = response;
   res.status(statusCode).json(body);
 };
