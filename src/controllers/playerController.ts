e37acf34c4a6527780be5614066076779a7e4857
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-13 18:59:40 -0300

    Create get player by id controller

--- src/controllers/playerController.ts
+++ src/controllers/playerController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
-import { getAllPlayersService } from "../services/playersService";
+import {
+  getAllPlayersService,
+  getPlayerByIdService,
+} from "../services/playersService";
 
 export const getAllPlayersController = async (req: Request, res: Response) => {
   const response = await getAllPlayersService();
+  const { statusCode, body } = response;
+  res.status(statusCode).json(body);
+};
+
+export const getPlayerByIdController = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+  const response = await getPlayerByIdService(id);
   const { statusCode, body } = response;
   res.status(statusCode).json(body);
 };
