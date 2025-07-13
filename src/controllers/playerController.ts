3152d95540c20c9eb9d799886de0f856483332a2
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-13 12:05:43 -0300

    Get players from service

--- src/controllers/playerController.ts
+++ src/controllers/playerController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
+import { getAllPlayersService } from "../services/playersService";
 
-export const getPlayer = (req: Request, res: Response) => {
-  res.status(200).json({
-    player: "Testing",
-  });
+export const getPlayer = async (req: Request, res: Response) => {
+  const data = await getAllPlayersService();
+  res.status(200).json(data);
 };
