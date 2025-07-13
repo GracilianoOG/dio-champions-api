4e45c5dab0577c916f2d885e44e703fc92bc40a2
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-13 19:00:02 -0300

    Add get player by id router

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getAllPlayersController } from "./controllers/playerController";
+import {
+  getAllPlayersController,
+  getPlayerByIdController,
+} from "./controllers/playerController";
 
 const router = Router();
 
 router.get("/players", getAllPlayersController);
+router.get("/players/:id", getPlayerByIdController);
 
 export default router;
