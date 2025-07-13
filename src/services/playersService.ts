8b59bfc3aa7211fa33918a3d2af7e27b8ee8af4a
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-13 18:59:19 -0300

    Create player by id service

--- src/services/playersService.ts
+++ src/services/playersService.ts
@@ -1,8 +1,21 @@
-import { findAllPlayers } from "../repositories/playerRepository";
+import {
+  findAllPlayers,
+  findPlayerById,
+} from "../repositories/playerRepository";
 import { NO_CONTENT, OK } from "../utils/httpHelper";
 
 export const getAllPlayersService = async () => {
   const data = await findAllPlayers();
+
+  if (!data) {
+    return await NO_CONTENT();
+  }
+
+  return await OK(data);
+};
+
+export const getPlayerByIdService = async (id: number) => {
+  const data = await findPlayerById(id);
 
   if (!data) {
     return await NO_CONTENT();
