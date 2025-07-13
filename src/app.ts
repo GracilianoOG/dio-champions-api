c82ce9b8f359e24348c5a63cb9980203b2ea852f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-13 11:46:04 -0300

    Get controller from playerController

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
 import express, { json, Express, Request, Response } from "express";
+import { getPlayer } from "./controllers/playerController";
 
 const createApp = (): Express => {
   const app = express();
 
   app.use(json());
 
-  app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({
-      player: "Testing",
-    });
-  });
+  app.get("/", getPlayer);
 
   return app;
 };
