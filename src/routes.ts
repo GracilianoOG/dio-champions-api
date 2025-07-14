3a6117c8109e3363d65ed1fc22907bd8f7ce89e1
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-14 15:25:33 -0300

    Add postPlayerController as a post method

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import {
   getAllPlayersController,
   getPlayerByIdController,
+  postPlayerController,
 } from "./controllers/playerController";
 
 const router = Router();
 
 router.get("/players", getAllPlayersController);
 router.get("/players/:id", getPlayerByIdController);
+
+router.post("/players", postPlayerController);
 
 export default router;
