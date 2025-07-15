31664dc9da713969ccf73cb5dba0660b511fddcc
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-15 18:17:44 -0300

    Use cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { json, Express } from "express";
 import router from "./routes";
+import cors from "cors";
 
 const createApp = (): Express => {
   const app = express();
 
+  app.use(cors());
   app.use(json());
   app.use("/api", router);
 
