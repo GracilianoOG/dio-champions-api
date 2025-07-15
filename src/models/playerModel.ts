944d855bbb59b595b54aafdb2d4cf143593b6da1
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-15 17:54:43 -0300

    Add club attribute

--- src/models/playerModel.ts
+++ src/models/playerModel.ts
@@ -3,5 +3,6 @@ import { PlayerStatisticsModel } from "./playerStatisticsModel";
 export interface PlayerModel {
   id: number;
   name: string;
+  club: string;
   statistics: PlayerStatisticsModel;
 }
