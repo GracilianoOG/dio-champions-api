82da0cc80a4f0bdc25202842a0238749144da12f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-16 19:42:28 -0300

    Create findClubById

--- src/repositories/clubRepository.ts
+++ src/repositories/clubRepository.ts
@@ -6,3 +6,11 @@ export const findAllClubs = async (): Promise<ClubModel[]> => {
   const clubs: ClubModel[] = JSON.parse(data);
   return clubs;
 };
+
+export const findClubById = async (
+  id: number
+): Promise<ClubModel | undefined> => {
+  const data = await fs.readFile("./src/data/clubs.json", "utf-8");
+  const clubs: ClubModel[] = JSON.parse(data);
+  return clubs.find(club => club.id === id);
+};
