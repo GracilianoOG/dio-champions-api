feee29c03632827425f473584ce17b988b78b7be
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-16 19:43:10 -0300

    Create getClubByIdController

--- src/controllers/clubController.ts
+++ src/controllers/clubController.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from "express";
-import { getAllClubsService } from "../services/clubService";
+import {
+  getAllClubsService,
+  getClubByIdService,
+} from "../services/clubService";
 
 export const getAllClubsController = async (req: Request, res: Response) => {
   const response = await getAllClubsService();
+  const { statusCode, body } = response;
+  res.status(statusCode).json(body);
+};
+
+export const getClubByIdController = async (req: Request, res: Response) => {
+  const clubId = parseInt(req.params.id);
+  const response = await getClubByIdService(clubId);
   const { statusCode, body } = response;
   res.status(statusCode).json(body);
 };
