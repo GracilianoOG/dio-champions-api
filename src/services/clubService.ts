d5edfd700d77deeceb7dcd727451afd23510b69a
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-16 19:42:51 -0300

    Create getClubByIdService

--- src/services/clubService.ts
+++ src/services/clubService.ts
@@ -1,9 +1,19 @@
-import { findAllClubs } from "../repositories/clubRepository";
-import { OK } from "../utils/httpHelper";
+import { findAllClubs, findClubById } from "../repositories/clubRepository";
+import { NO_CONTENT, OK } from "../utils/httpHelper";
 
 export const getAllClubsService = async () => {
   const data = await findAllClubs();
 
   const response = OK(data);
   return response;
+};
+
+export const getClubByIdService = async (id: number) => {
+  const data = await findClubById(id);
+
+  if (!data) {
+    return await NO_CONTENT();
+  }
+
+  return await OK(data);
 };
