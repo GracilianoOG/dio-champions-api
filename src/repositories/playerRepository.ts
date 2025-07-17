8c312d0844edb7039a13168a287a336d97ff5cdf
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-17 16:12:13 -0300

    Rename to database

--- src/repositories/playerRepository.ts
+++ src/repositories/playerRepository.ts
@@ -2,27 +2,27 @@ import { players } from "../data/players";
 import { PlayerModel } from "../models/playerModel";
 import { PlayerStatisticsModel } from "../models/playerStatisticsModel";
 
-const dataBase: PlayerModel[] = players;
+const database: PlayerModel[] = players;
 
 export const findAllPlayers = async (): Promise<PlayerModel[]> => {
-  return dataBase;
+  return database;
 };
 
 export const findPlayerById = async (
   id: number
 ): Promise<PlayerModel | undefined> => {
-  return dataBase.find(player => player.id === id);
+  return database.find(player => player.id === id);
 };
 
 export const insertPlayer = async (player: PlayerModel) => {
-  dataBase.push(player);
+  database.push(player);
 };
 
 export const deletePlayer = async (id: number) => {
-  const index = dataBase.findIndex(player => player.id === id);
+  const index = database.findIndex(player => player.id === id);
 
   if (index !== -1) {
-    dataBase.splice(index, 1);
+    database.splice(index, 1);
     return true;
   }
 
@@ -33,14 +33,14 @@ export const updatePlayerStatistics = async (
   id: number,
   statistics: PlayerStatisticsModel
 ) => {
-  const playerIndex = dataBase.findIndex(player => player.id === id);
+  const playerIndex = database.findIndex(player => player.id === id);
 
   if (playerIndex !== -1) {
-    dataBase[playerIndex].statistics = statistics;
-    return dataBase[playerIndex];
+    database[playerIndex].statistics = statistics;
+    return database[playerIndex];
   }
 };
 
 export const filterPlayersByClub = async (club: string) => {
-  return dataBase.filter(player => player.club === club);
+  return database.filter(player => player.club === club);
 };
