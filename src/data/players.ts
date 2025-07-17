1e41398da6e71a422c58c82e51d5bb005429ea2a
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-17 16:11:16 -0300

    Create players list

---
+++ src/data/players.ts
@@ -0,0 +1,204 @@
+import { PlayerModel } from "../models/playerModel";
+
+export const players: PlayerModel[] = [
+  {
+    id: 1,
+    name: "Lionel Messi",
+    club: "Inter Miami",
+    statistics: {
+      strength: 65,
+      speed: 90,
+      technique: 95,
+    },
+  },
+  {
+    id: 2,
+    name: "Cristiano Ronaldo",
+    club: "Al Nassr",
+    statistics: {
+      strength: 85,
+      speed: 88,
+      technique: 88,
+    },
+  },
+  {
+    id: 3,
+    name: "Kylian Mbappé",
+    club: "Paris Saint-Germain",
+    statistics: {
+      strength: 70,
+      speed: 95,
+      technique: 80,
+    },
+  },
+  {
+    id: 4,
+    name: "Karim Benzema",
+    club: "Al-Ittihad",
+    statistics: {
+      strength: 80,
+      speed: 78,
+      technique: 90,
+    },
+  },
+  {
+    id: 5,
+    name: "Kevin De Bruyne",
+    club: "Manchester City",
+    statistics: {
+      strength: 70,
+      speed: 75,
+      technique: 92,
+    },
+  },
+  {
+    id: 6,
+    name: "Mohamed Salah",
+    club: "Liverpool",
+    statistics: {
+      strength: 75,
+      speed: 92,
+      technique: 85,
+    },
+  },
+  {
+    id: 7,
+    name: "Virgil van Dijk",
+    club: "Liverpool",
+    statistics: {
+      strength: 90,
+      speed: 70,
+      technique: 75,
+    },
+  },
+  {
+    id: 8,
+    name: "Neymar Jr.",
+    club: "Al-Hilal",
+    statistics: {
+      strength: 60,
+      speed: 85,
+      technique: 93,
+    },
+  },
+  {
+    id: 9,
+    name: "Erling Haaland",
+    club: "Manchester City",
+    statistics: {
+      strength: 85,
+      speed: 89,
+      technique: 88,
+    },
+  },
+  {
+    id: 10,
+    name: "Luka Modrić",
+    club: "Real Madrid",
+    statistics: {
+      strength: 65,
+      speed: 70,
+      technique: 90,
+    },
+  },
+  {
+    id: 11,
+    name: "Robert Lewandowski",
+    club: "Barcelona",
+    statistics: {
+      strength: 80,
+      speed: 78,
+      technique: 92,
+    },
+  },
+  {
+    id: 12,
+    name: "Joshua Kimmich",
+    club: "Bayern de Munique",
+    statistics: {
+      strength: 70,
+      speed: 72,
+      technique: 90,
+    },
+  },
+  {
+    id: 13,
+    name: "Sadio Mané",
+    club: "Bayern de Munique",
+    statistics: {
+      strength: 75,
+      speed: 90,
+      technique: 85,
+    },
+  },
+  {
+    id: 14,
+    name: "Antoine Griezmann",
+    club: "Atlético de Madrid",
+    statistics: {
+      strength: 65,
+      speed: 80,
+      technique: 88,
+    },
+  },
+  {
+    id: 15,
+    name: "Thibaut Courtois",
+    club: "Real Madrid",
+    statistics: {
+      strength: 70,
+      speed: 65,
+      technique: 85,
+    },
+  },
+  {
+    id: 16,
+    name: "Jude Bellingham",
+    club: "Real Madrid",
+    statistics: {
+      strength: 70,
+      speed: 85,
+      technique: 88,
+    },
+  },
+  {
+    id: 17,
+    name: "Phil Foden",
+    club: "Manchester City",
+    statistics: {
+      strength: 60,
+      speed: 85,
+      technique: 90,
+    },
+  },
+  {
+    id: 18,
+    name: "Pedri",
+    club: "Barcelona",
+    statistics: {
+      strength: 55,
+      speed: 80,
+      technique: 92,
+    },
+  },
+  {
+    id: 19,
+    name: "Gianluigi Donnarumma",
+    club: "Paris Saint-Germain",
+    statistics: {
+      strength: 65,
+      speed: 60,
+      technique: 85,
+    },
+  },
+  {
+    id: 20,
+    name: "João Félix",
+    club: "Chelsea",
+    statistics: {
+      strength: 60,
+      speed: 85,
+      technique: 88,
+    },
+  },
+];
